2af93a095f80ccdc34bcfb1336551faad0702bf6
tgmendes <tmendes@gocardless.com>
2025-08-23 10:07:25 +0100

    Add code to handle generation API requests

---
+++ generation.go
@@ -0,0 +1,149 @@
+package pokeapi
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/url"
+
+	"github.com/tgmendes/pokeapi-sdk/internal/apitype"
+)
+
+type Generation struct {
+	ID             int
+	Name           string
+	Region         string
+	Locations      []string
+	Moves          []string
+	PokemonSpecies []string
+	Types          []string
+}
+
+const generationPath = "/generation"
+
+func (c *Client) GenerationByID(ctx context.Context, id int) (*Generation, error) {
+	path, err := url.JoinPath(generationPath, fmt.Sprint(id))
+	if err != nil {
+		return nil, fmt.Errorf("failed to join path: %w", err)
+	}
+
+	var apiGeneration apitype.Generation
+	err = c.Get(ctx, path, &apiGeneration)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get generation with id %d: %w", id, err)
+	}
+
+	return c.mapGeneration(ctx, apiGeneration)
+}
+
+func (c *Client) GenerationByName(ctx context.Context, name string) (*Generation, error) {
+	path, err := url.JoinPath(generationPath, name)
+	if err != nil {
+		return nil, fmt.Errorf("failed to join path: %w", err)
+	}
+
+	var apiGeneration apitype.Generation
+	err = c.Get(ctx, path, &apiGeneration)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get generation with name %s: %w", name, err)
+	}
+
+	return c.mapGeneration(ctx, apiGeneration)
+}
+
+func (c *Client) AllGenerations(ctx context.Context, options ...RequestOption) ([]Generation, error) {
+	pager := c.GenerationPager(options...)
+
+	var results []Generation
+	for {
+		list, err := pager.Next(ctx)
+		if errors.Is(err, ErrNoMorePages) {
+			break
+		}
+
+		if err != nil {
+			return nil, err
+		}
+
+		apiGenerations, err := FetchResultsN[apitype.Generation](ctx, c, list, 5)
+		if err != nil {
+			return nil, err
+		}
+
+		generations := make([]Generation, 0, len(apiGenerations))
+		for _, apiGeneration := range apiGenerations {
+			generation, err := c.mapGeneration(ctx, apiGeneration)
+			if err != nil {
+				return nil, err
+			}
+			generations = append(generations, *generation)
+		}
+
+		results = append(results, generations...)
+	}
+
+	return results, nil
+}
+
+func (c *Client) GenerationPage(ctx context.Context, options ...RequestOption) ([]Resource, error) {
+	pager := c.GenerationPager(options...)
+
+	return pager.Next(ctx)
+}
+
+func (c *Client) GenerationPager(options ...RequestOption) *Pager {
+	opts := defaultRequestOptions()
+	if options != nil {
+		opts = processOptions(options...)
+	}
+
+	q := opts.urlParams.Encode()
+	startPath := generationPath
+	if q != "" {
+		startPath = fmt.Sprintf("%s?%s", generationPath, q)
+	}
+
+	return NewPager(c, startPath)
+}
+
+func (c *Client) mapGeneration(ctx context.Context, gen apitype.Generation) (*Generation, error) {
+	moves := make([]string, 0, len(gen.Moves))
+	for _, move := range gen.Moves {
+		moves = append(moves, move.Name)
+	}
+
+	species := make([]string, 0, len(gen.PokemonSpecies))
+	for _, apiSpecies := range gen.PokemonSpecies {
+		species = append(species, apiSpecies.Name)
+	}
+
+	pokeTypes := make([]string, 0, len(gen.Types))
+	for _, apiType := range gen.Types {
+		pokeTypes = append(pokeTypes, apiType.Name)
+	}
+
+	regionResource := Resource{
+		Name: gen.MainRegion.Name,
+		URL:  gen.MainRegion.URL,
+	}
+
+	region, err := FetchResults[apitype.Region](ctx, c, []Resource{regionResource})
+	if err != nil {
+		return nil, err
+	}
+
+	locationNames := make([]string, 0, len(region[0].Locations))
+	for _, location := range region[0].Locations {
+		locationNames = append(locationNames, location.Name)
+	}
+
+	return &Generation{
+		ID:             gen.ID,
+		Name:           gen.Name,
+		Region:         gen.MainRegion.Name,
+		Locations:      locationNames,
+		Moves:          moves,
+		PokemonSpecies: species,
+		Types:          pokeTypes,
+	}, nil
+}
